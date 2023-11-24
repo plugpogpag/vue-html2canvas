3d8eff0fd16a02bd03a8492db3896690b5af1bcb
krittapoj Lertriddecha <ktp_1999@hotmail.com>
2023-11-24 17:51:49 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 
 const VueHtml2Canvas = {
-  install (Vue, options) {
-    Vue.mixin({
-      methods: {
-        async $html2canvas (el, options = {}) {
-          const { type } = options;
-          const canvas = await html2canvas(el, options);
-          if (type && type === 'dataURL') { return canvas.toDataURL(); } else {
-            console.warn('Vue Html2Canvas Warn: Invalid option type. Use \'dataURL\' instead. Returning canvas.');
-            return canvas;
-          }
-        }
+  install(Vue, options) {
+    async function $html2canvas(el, options = {}) {
+      const { type } = options;
+      const canvas = await html2canvas(el, options);
+      if (type && type === "dataURL") {
+        return canvas.toDataURL();
+      } else {
+        console.warn(
+          "Vue Html2Canvas Warn: Invalid option type. Use 'dataURL' instead. Returning canvas."
+        );
+        return canvas;
       }
-    });
-  }
+    }
+    Vue.provide("$html2canvas", $html2canvas);
+  },
 };
 
 export default VueHtml2Canvas;
